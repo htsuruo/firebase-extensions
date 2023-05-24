cf730cbdf53b47f973734ae441fc0442ea71a4bb
tsuruo <tsuru.dev@gmail.com>
2023-05-24 20:44:03 +0900

    Change scheduled trigger

--- extensions/backup-firestore-to-storage/functions/src/index.ts
+++ extensions/backup-firestore-to-storage/functions/src/index.ts
@@ -1,27 +1,27 @@
-import { https, logger } from 'firebase-functions/v2'
+import { pubsub, logger } from 'firebase-functions/v1'
 import { storage } from 'firebase-admin'
 import { v1 } from '@google-cloud/firestore'
 const client = new v1.FirestoreAdminClient()
 
 // ref. https://firebase.google.com/docs/firestore/solutions/schedule-export?hl=ja
-exports.backupTransaction = https.onRequest(async (req: https.Request, res) => {
-  const projectId = process.env.GCP_PROJECT || process.env.GCLOUD_PROJECT!
-  const bucketName = process.env.BUCKET_NAME!
-  const outputUriPrefix = `gs://${bucketName}/${process.env.PATH}`
-  const databaseName = client.databasePath(projectId, '(default)')
-  try {
-    await createBucketUnlessExists(bucketName)
-    await client.exportDocuments({
-      name: databaseName,
-      outputUriPrefix: outputUriPrefix,
-      collectionIds: process.env.COLLECTION_IDS?.split(','),
-    })
-    res.sendStatus(200)
-  } catch (error) {
-    logger.error(error)
-    res.sendStatus(500)
-  }
-})
+exports.backupTransaction = pubsub
+  .schedule(process.env.SCHEDULE_FREQUENCY!)
+  .onRun(async (_) => {
+    const projectId = process.env.GCP_PROJECT || process.env.GCLOUD_PROJECT!
+    const bucketName = process.env.BUCKET_NAME!
+    const outputUriPrefix = `gs://${bucketName}/${process.env.PATH}`
+    const databaseName = client.databasePath(projectId, '(default)')
+    try {
+      await createBucketUnlessExists(bucketName)
+      await client.exportDocuments({
+        name: databaseName,
+        outputUriPrefix: outputUriPrefix,
+        collectionIds: process.env.COLLECTION_IDS?.split(','),
+      })
+    } catch (error) {
+      logger.error(error)
+    }
+  })
 
 // Check if the bucket exists and create it if not
 async function createBucketUnlessExists(bucketName: string) {
