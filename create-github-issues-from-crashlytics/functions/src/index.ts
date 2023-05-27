6290ff2d4b6cd16c6ca62d091193e0f88caa328c
tsuruo <tsuru.dev@gmail.com>
2023-05-28 01:27:34 +0900

    Add ALERTS param

--- create-github-issues-from-crashlytics/functions/src/index.ts
+++ create-github-issues-from-crashlytics/functions/src/index.ts
@@ -1,21 +1,34 @@
 // ref. https://firebase.google.com/docs/functions/beta/custom-events?hl=ja#handle-events
 import { logger, alerts } from 'firebase-functions/v2'
 
-exports.createNewFatalIssue = alerts.crashlytics.onNewFatalIssuePublished(
-  {
-    region: process.env.LOCATION,
-    secrets: ['GITHUB_ACCESS_TOKEN'],
-  },
+const options = {
+  region: process.env.LOCATION,
+  secrets: ['GITHUB_ACCESS_TOKEN'],
+}
+
+const ownerRepo = `${process.env.GITHUB_OWNER}/${process.env.GITHUB_REPO}`
+const labels = `${process.env.GITHUB_LABELS}`
+
+exports.createFatalIssue = alerts.crashlytics.onNewFatalIssuePublished(
+  options,
   (event) => {
-    const ownerRepo = `${process.env.GITHUB_OWNER}/${process.env.GITHUB_REPO}`
-    const labels = `${process.env.GITHUB_LABELS}`
     logger.info(`ownerRepo: ${ownerRepo}, labels: ${labels}`)
-    logger.info('createNewFatalIssue event', event)
+    logger.info('NewFatalIssue event', event)
     const appId = event.appId
     logger.info(`appId: ${appId}`)
     logger.info(event.data.payload.issue, { structuredData: true })
     // const { id, title, subtitle, appVersion } = event.data.payload.issue
     // ref. https://docs.github.com/ja/rest/issues/issues?apiVersion=2022-11-28#create-an-issue
     // TODO(tsuruoka): GitHub API叩く
+  }
+)
+
+exports.createNonFatalIssue = alerts.crashlytics.onNewNonfatalIssuePublished(
+  options,
+  (event) => {
+    logger.info(`ownerRepo: ${ownerRepo}, labels: ${labels}`)
+    const appId = event.appId
+    logger.info(`appId: ${appId}`)
+    logger.info(event.data.payload.issue, { structuredData: true })
   }
 )
