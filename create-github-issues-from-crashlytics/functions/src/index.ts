09ba275b9605e6e0e5b12c7d5c290e19c457a2da
tsuruo <tsuru.dev@gmail.com>
2023-05-27 14:35:26 +0900

    Chnage to use alerts of firebase-function package

--- create-github-issues-from-crashlytics/functions/src/index.ts
+++ create-github-issues-from-crashlytics/functions/src/index.ts
@@ -1,10 +1,8 @@
 // ref. https://firebase.google.com/docs/functions/beta/custom-events?hl=ja#handle-events
-import { logger, eventarc } from 'firebase-functions/v2'
+import { logger, alerts } from 'firebase-functions/v2'
 
-// ref. https://docs.github.com/ja/rest/issues/issues?apiVersion=2022-11-28#create-an-issue
-exports.createNewFatalIssue = eventarc.onCustomEventPublished(
+exports.createNewFatalIssue = alerts.crashlytics.onNewFatalIssuePublished(
   {
-    eventType: 'google.firebase.firebasealerts.alerts.v1.published',
     region: process.env.LOCATION,
     secrets: ['GITHUB_ACCESS_TOKEN'],
   },
@@ -13,5 +11,11 @@ exports.createNewFatalIssue = eventarc.onCustomEventPublished(
     const labels = `${process.env.GITHUB_LABELS}`
     logger.info(`ownerRepo: ${ownerRepo}, labels: ${labels}`)
     logger.info('createNewFatalIssue event', event)
+    const appId = event.appId
+    logger.info(`appId: ${appId}`)
+    logger.info(event.data.payload.issue, { structuredData: true })
+    // const { id, title, subtitle, appVersion } = event.data.payload.issue
+    // ref. https://docs.github.com/ja/rest/issues/issues?apiVersion=2022-11-28#create-an-issue
+    // TODO(tsuruoka): GitHub API叩く
   }
 )
