0db499064553f7260fa1d7ae056d2ada927fd5e0
tsuruo <tsuru.dev@gmail.com>
2023-05-27 13:56:54 +0900

    Create eventarc listen function

--- create-github-issues-from-crashlytics/functions/src/index.ts
+++ create-github-issues-from-crashlytics/functions/src/index.ts
@@ -1,28 +1,17 @@
-/*
- * This template contains a HTTP function that responds
- * with a greeting when called
- *
- * Reference PARAMETERS in your functions code with:
- * `process.env.<parameter-name>`
- * Learn more about building extensions in the docs:
- * https://firebase.google.com/docs/extensions/publishers
- */
-
-import * as functions from 'firebase-functions'
+// ref. https://firebase.google.com/docs/functions/beta/custom-events?hl=ja#handle-events
+import { logger, eventarc } from 'firebase-functions/v2'
 
 // ref. https://docs.github.com/ja/rest/issues/issues?apiVersion=2022-11-28#create-an-issue
-exports.greetTheWorld = functions.https.onRequest(
-  (req: functions.Request, res: functions.Response) => {
-    // Here we reference a user-provided parameter
-    // (its value is provided by the user during installation)
-    const consumerProvidedGreeting = process.env.GREETING
-
-    // And here we reference an auto-populated parameter
-    // (its value is provided by Firebase after installation)
-    const instanceId = process.env.EXT_INSTANCE_ID
-
-    const greeting = `${consumerProvidedGreeting} World from ${instanceId}`
-
-    res.send(greeting)
+exports.createNewFatalIssue = eventarc.onCustomEventPublished(
+  {
+    eventType: 'google.firebase.firebasealerts.alerts.v1.published',
+    region: process.env.LOCATION,
+    secrets: ['GITHUB_ACCESS_TOKEN'],
+  },
+  (event) => {
+    const ownerRepo = `${process.env.GITHUB_OWNER}/${process.env.GITHUB_REPO}`
+    const labels = `${process.env.GITHUB_LABELS}`
+    logger.info(`ownerRepo: ${ownerRepo}, labels: ${labels}`)
+    logger.info('createNewFatalIssue event', event)
   }
 )
