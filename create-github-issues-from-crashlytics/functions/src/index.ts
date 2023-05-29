061297275636ce0899836e947761e7f8b3547613
tsuruo <tsuru.dev@gmail.com>
2023-05-29 12:56:09 +0900

    Refactor

--- create-github-issues-from-crashlytics/functions/src/index.ts
+++ create-github-issues-from-crashlytics/functions/src/index.ts
@@ -1,5 +1,9 @@
 // ref. https://firebase.google.com/docs/functions/beta/custom-events?hl=ja#handle-events
-import { logger, alerts } from 'firebase-functions/v2'
+import { logger } from 'firebase-functions/v2'
+import {
+  onNewFatalIssuePublished,
+  onNewNonfatalIssuePublished,
+} from 'firebase-functions/v2/alerts/crashlytics'
 
 const options = {
   region: process.env.LOCATION,
@@ -9,26 +13,20 @@ const options = {
 const ownerRepo = `${process.env.GITHUB_OWNER}/${process.env.GITHUB_REPO}`
 const labels = `${process.env.GITHUB_LABELS}`
 
-exports.createFatalIssue = alerts.crashlytics.onNewFatalIssuePublished(
-  options,
-  (event) => {
-    logger.info(`ownerRepo: ${ownerRepo}, labels: ${labels}`)
-    logger.info('NewFatalIssue event', event)
-    const appId = event.appId
-    logger.info(`appId: ${appId}`)
-    logger.info(event.data.payload.issue, { structuredData: true })
-    // const { id, title, subtitle, appVersion } = event.data.payload.issue
-    // ref. https://docs.github.com/ja/rest/issues/issues?apiVersion=2022-11-28#create-an-issue
-    // TODO(tsuruoka): GitHub API叩く
-  }
-)
+exports.createFatalIssue = onNewFatalIssuePublished(options, (event) => {
+  logger.info(`ownerRepo: ${ownerRepo}, labels: ${labels}`)
+  logger.info('NewFatalIssue event', event)
+  const appId = event.appId
+  logger.info(`appId: ${appId}`)
+  logger.info(event.data.payload.issue, { structuredData: true })
+  // const { id, title, subtitle, appVersion } = event.data.payload.issue
+  // ref. https://docs.github.com/ja/rest/issues/issues?apiVersion=2022-11-28#create-an-issue
+  // TODO(tsuruoka): GitHub API叩く
+})
 
-exports.createNonFatalIssue = alerts.crashlytics.onNewNonfatalIssuePublished(
-  options,
-  (event) => {
-    logger.info(`ownerRepo: ${ownerRepo}, labels: ${labels}`)
-    const appId = event.appId
-    logger.info(`appId: ${appId}`)
-    logger.info(event.data.payload.issue, { structuredData: true })
-  }
-)
+exports.createNonFatalIssue = onNewNonfatalIssuePublished(options, (event) => {
+  logger.info(`ownerRepo: ${ownerRepo}, labels: ${labels}`)
+  const appId = event.appId
+  logger.info(`appId: ${appId}`)
+  logger.info(event.data.payload.issue, { structuredData: true })
+})
