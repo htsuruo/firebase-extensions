b93dd0874b9508a4ec2546bcd64ef322a5aee520
tsuruo <tsuru.dev@gmail.com>
2023-06-01 12:08:49 +0900

    Refactor using generics

---
+++ trigger-github-issues-from-crashlytics/functions/src/types.ts
@@ -0,0 +1,20 @@
+import {
+  NewAnrIssuePayload,
+  NewFatalIssuePayload,
+  NewNonfatalIssuePayload,
+} from 'firebase-functions/v2/alerts/crashlytics'
+
+export type CrashlyticsAlert =
+  | 'newFatalIssue'
+  | 'newNonfatalIssue'
+  | 'newAnrIssue'
+  | 'regression'
+  | 'stabilityDigest'
+  | 'velocity'
+
+// イベントトリガーで渡ってくるPayloadクラスには継承関係が無い（親クラスを持っていない）ので、
+// ジェネリクスで利用できるようにtypeで束ねる
+export type CrashlyticsPayload =
+  | NewFatalIssuePayload
+  | NewNonfatalIssuePayload
+  | NewAnrIssuePayload
