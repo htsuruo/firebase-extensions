7da01b33160ef3b86c49a6d45a79275cf6502aa7
tsuruo <tsuru.dev@gmail.com>
2023-06-02 18:07:42 +0900

    Remove commentout

--- back-up-firestore-to-storage/functions/src/index.ts
+++ back-up-firestore-to-storage/functions/src/index.ts
@@ -1,10 +1,9 @@
 import { pubsub, logger } from 'firebase-functions/v1'
-// import { storage, initializeApp } from 'firebase-admin'
+import { storage, initializeApp } from 'firebase-admin'
 import { v1 } from '@google-cloud/firestore'
-const client = new v1.FirestoreAdminClient()
-
 // TODO(tsuruoka): TypeError: Cannot read properties of undefined (reading 'INTERNAL')
-// initializeApp()
+initializeApp()
+const client = new v1.FirestoreAdminClient()
 
 // ref. https://firebase.google.com/docs/firestore/solutions/schedule-export?hl=ja
 exports.backupTransaction = pubsub
@@ -13,11 +12,11 @@ exports.backupTransaction = pubsub
   .onRun(async (_) => {
     const projectId = process.env.PROJECT_ID!
     const databaseName = client.databasePath(projectId, '(default)')
-    const bucketName = process.env.BUCKET_NAME ?? process.env.STORAGE_BUCKET
+    const bucketName = process.env.BUCKET_NAME ?? process.env.STORAGE_BUCKET!
     const path = process.env.PATH ?? process.env.EXT_INSTANCE_ID
     const outputUriPrefix = `gs://${bucketName}/${path}`
     try {
-      // await createBucketUnlessExists(bucketName)
+      await createBucketUnlessExists(bucketName)
       await client.exportDocuments({
         name: databaseName,
         outputUriPrefix: outputUriPrefix,
@@ -32,10 +31,10 @@ exports.backupTransaction = pubsub
   })
 
 // Check if the bucket exists and create it if not
-// async function createBucketUnlessExists(bucketName: string) {
-//   const bucket = storage().bucket(bucketName)
-//   const [exists] = await bucket.exists()
-//   if (!exists) {
-//     await bucket.create()
-//   }
-// }
+async function createBucketUnlessExists(bucketName: string) {
+  const bucket = storage().bucket(bucketName)
+  const [exists] = await bucket.exists()
+  if (!exists) {
+    await bucket.create()
+  }
+}
