17a7e16e1d7cb7a67277024e093f95afac27c203
tsuruo <tsuru.dev@gmail.com>
2023-06-03 21:24:08 +0900

    wip: test with local emulator

--- back-up-firestore-to-storage/functions/integration-tests/debug.sh
+++ back-up-firestore-to-storage/functions/integration-tests/debug.sh
@@ -1,1 +1,3 @@
-firebase emulators:start --import=./data --project=demo-test --inspect-functions
+# Google Cloud APIを利用するため、実プロジェクトでのテストが必要
+# 手元の環境に合わせて.firebasercのprojectを変更する必要があります。
+firebase emulators:start --import=./data  --inspect-functions
--- back-up-firestore-to-storage/functions/src/index.ts
+++ back-up-firestore-to-storage/functions/src/index.ts
@@ -1,8 +1,8 @@
-import { pubsub, logger } from 'firebase-functions/v1'
-import { initializeApp } from 'firebase-admin'
+import { pubsub, logger, https } from 'firebase-functions/v1'
+// import { initializeApp } from 'firebase-admin'
 import { v1 } from '@google-cloud/firestore'
 
-const app = initializeApp()
+// const app = initializeApp()
 const client = new v1.FirestoreAdminClient()
 
 // ref. https://firebase.google.com/docs/firestore/solutions/schedule-export?hl=ja
@@ -15,7 +15,7 @@ exports.backupTransaction = pubsub
     const path = process.env.PATH ?? process.env.EXT_INSTANCE_ID
     const outputUriPrefix = `gs://${bucketName}/${path}`
     try {
-      await createBucketUnlessExists(bucketName)
+      // await createBucketIfNotFound(bucketName)
       await client.exportDocuments({
         name: databaseName,
         outputUriPrefix: outputUriPrefix,
@@ -29,11 +29,33 @@ exports.backupTransaction = pubsub
     }
   })
 
-// Check if the bucket exists and create it if not
-async function createBucketUnlessExists(bucketName: string) {
-  const bucket = app.storage().bucket(bucketName)
-  const [exists] = await bucket.exists()
-  if (!exists) {
-    await bucket.create()
+exports.backupTransactionHttps = https.onRequest(async (_req, _resp) => {
+  const projectId = process.env.PROJECT_ID!
+  const databaseName = client.databasePath(projectId, '(default)')
+  const bucketName = process.env.BUCKET_NAME!
+  let outputUriPrefix = `gs://${bucketName}`
+
+  try {
+    // await createBucketIfNotFound(bucketName)
+    const res = await client.exportDocuments({
+      name: databaseName,
+      collectionIds: process.env.COLLECTION_IDS?.split(','),
+      outputUriPrefix: outputUriPrefix,
+    })
+    logger.info(res, { structuredData: true })
+    logger.info(`✅ Backup ${databaseName} to ${outputUriPrefix} successfully.`)
+  } catch (error) {
+    logger.error(error)
   }
-}
+})
+
+// TODO(tsuruoka): Google Cloud のSDKを使ってバケットを作成する(=firebase-adminでは作成できないぽい)
+// Check if the bucket exists and create it if not
+// async function createBucketIfNotFound(bucketName: string) {
+//   const bucket = app.storage().bucket(bucketName)
+//   const [exists] = await bucket.exists()
+//   if (!exists) {
+//     bucket.create()
+//     logger.info('not extists, create bucket')
+//   }
+// }
