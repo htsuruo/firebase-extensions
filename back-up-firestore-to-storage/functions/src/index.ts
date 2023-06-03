1dd94848f3f13f1e0ec9d06c55318ff4920740ac
tsuruo <tsuru.dev@gmail.com>
2023-06-04 03:13:40 +0900

    Use googleapis Storage

--- back-up-firestore-to-storage/functions/src/index.ts
+++ back-up-firestore-to-storage/functions/src/index.ts
@@ -1,9 +1,9 @@
 import { pubsub, logger, https } from 'firebase-functions/v1'
-// import { initializeApp } from 'firebase-admin'
 import { v1 } from '@google-cloud/firestore'
+// import { Storage } from '@google-cloud/storage'
 
-// const app = initializeApp()
 const client = new v1.FirestoreAdminClient()
+// const storage = new Storage()
 
 // ref. https://firebase.google.com/docs/firestore/solutions/schedule-export?hl=ja
 exports.backupTransaction = pubsub
@@ -29,7 +29,7 @@ exports.backupTransaction = pubsub
     }
   })
 
-exports.backupTransactionHttps = https.onRequest(async (_req, _resp) => {
+exports.backupTransactionHttps = https.onRequest(async (_req, res) => {
   const projectId = process.env.PROJECT_ID!
   const databaseName = client.databasePath(projectId, '(default)')
   const bucketName = process.env.BUCKET_NAME!
@@ -37,25 +37,32 @@ exports.backupTransactionHttps = https.onRequest(async (_req, _resp) => {
 
   try {
     // await createBucketIfNotFound(bucketName)
-    const res = await client.exportDocuments({
+    await client.exportDocuments({
       name: databaseName,
       collectionIds: process.env.COLLECTION_IDS?.split(','),
       outputUriPrefix: outputUriPrefix,
     })
-    logger.info(res, { structuredData: true })
     logger.info(`✅ Backup ${databaseName} to ${outputUriPrefix} successfully.`)
   } catch (error) {
     logger.error(error)
   }
 })
 
-// TODO(tsuruoka): Google Cloud のSDKを使ってバケットを作成する(=firebase-adminでは作成できないぽい)
+// TODO(tsuruoka): ApiError: Not Implemented
+
 // Check if the bucket exists and create it if not
+//
+// The reason why we need to use googleapis instead of firebase-admin SDK is
+// Cloud Storage for Firebase does not support `Bucket` APIs.
+// ref. https://firebase.google.com/docs/emulator-suite/connect_storage#differences_from_google_cloud_storage
 // async function createBucketIfNotFound(bucketName: string) {
-//   const bucket = app.storage().bucket(bucketName)
+//   const bucket = storage.bucket(bucketName)
 //   const [exists] = await bucket.exists()
 //   if (!exists) {
-//     bucket.create()
-//     logger.info('not extists, create bucket')
+//     const [bucket] = await storage.createBucket(bucketName, {
+//       location: process.env.LOCATION,
+//       coldline: true,
+//     })
+//     logger.info(`${bucket.name} created with coldline in ${location}`)
 //   }
 // }
