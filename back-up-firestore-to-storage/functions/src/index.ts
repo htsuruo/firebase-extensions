738dd798b77b9f788fb9ed499edaa21b8c8d759e
tsuruo <tsuru.dev@gmail.com>
2023-06-08 22:06:38 +0900

    Remove unused comment
    - 作成されているバケットから選択する形式にしたため作成処理は不要になった

--- back-up-firestore-to-storage/functions/src/index.ts
+++ back-up-firestore-to-storage/functions/src/index.ts
@@ -2,10 +2,8 @@ import { pubsub, logger } from 'firebase-functions/v1'
 import { v1 } from '@google-cloud/firestore'
 import { HttpsError } from 'firebase-functions/v1/https'
 import { formatTimestamp } from './format'
-// import { Storage } from '@google-cloud/storage'
 
 const client = new v1.FirestoreAdminClient()
-// const storage = new Storage()
 
 // ref. https://firebase.google.com/docs/firestore/solutions/schedule-export?hl=en
 exports.backupTransaction = pubsub
@@ -26,7 +24,6 @@ exports.backupTransaction = pubsub
     })}`
 
     try {
-      // await createBucketIfNotFound(bucketName)
       await client.exportDocuments({
         name: databaseName,
         collectionIds: process.env.COLLECTION_IDS?.split(','),
@@ -40,24 +37,3 @@ exports.backupTransaction = pubsub
       throw new HttpsError('internal', '🚨 Backup operation failed.')
     }
   })
-
-// TODO(tsuruoka): バケット作成のAPIを叩いているはずが`ApiError: Not Implemented`となり作成できない問題
-// そもそもバケットの作成については最上位権限の`storage.admin`のIAM roleが必要なので、このためだけに付与するのはセキュリティ的には微妙な気がしている
-// 利用者側にセットアップしてもらうほうが良い気がしている。
-
-// Check if the bucket exists and create it if not
-//
-// The reason why we need to use googleapis instead of firebase-admin SDK is
-// Cloud Storage for Firebase does not support `Bucket` APIs.
-// ref. https://firebase.google.com/docs/emulator-suite/connect_storage#differences_from_google_cloud_storage
-// async function createBucketIfNotFound(bucketName: string) {
-//   const bucket = storage.bucket(bucketName)
-//   const [exists] = await bucket.exists()
-//   if (!exists) {
-//     const [bucket] = await storage.createBucket(bucketName, {
-//       location: process.env.LOCATION,
-//       coldline: true,
-//     })
-//     logger.info(`${bucket.name} created with coldline in ${location}`)
-//   }
-// }
