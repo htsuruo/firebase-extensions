6e675286b7f89b695ae39885e509c5a0b2407261
tsuruo <tsuru.dev@gmail.com>
2023-06-15 17:55:41 +0900

    Add retryWithUniqueSuffix func to avoid name collision

--- back-up-firestore-to-storage/functions/src/index.ts
+++ back-up-firestore-to-storage/functions/src/index.ts
@@ -6,14 +6,14 @@ import { formatTimestamp } from './format'
 
 const client = new v1.FirestoreAdminClient()
 // const storage = new Storage()
+const projectId = process.env.PROJECT_ID!
+const databaseName = client.databasePath(projectId, '(default)')
+const bucketName = process.env.BUCKET_NAME ?? process.env.STORAGE_BUCKET
 
 // ref. https://firebase.google.com/docs/firestore/solutions/schedule-export?hl=en
 exports.backupTransaction = pubsub
   .schedule(`'${process.env.SCHEDULE!}'`)
   .onRun(async (context) => {
-    const projectId = process.env.PROJECT_ID!
-    const databaseName = client.databasePath(projectId, '(default)')
-    const bucketName = process.env.BUCKET_NAME ?? process.env.STORAGE_BUCKET
     let outputUriPrefix = `gs://${bucketName}`
 
     const prefixPath = process.env.PREFIX_PATH
@@ -26,17 +26,44 @@ exports.backupTransaction = pubsub
       format: process.env.TIMESTAMP_FORMAT,
     })}`
 
-    try {
-      await client.exportDocuments({
-        name: databaseName,
-        collectionIds: process.env.COLLECTION_IDS?.split(','),
-        outputUriPrefix: outputUriPrefix,
-      })
-      logger.info(
-        `✅ Backup ${databaseName} to ${outputUriPrefix} successfully.`
-      )
-    } catch (error) {
-      logger.error(error, { structuredData: true })
-      throw new HttpsError('internal', '🚨 Backup operation failed.')
-    }
+    await exportDocuments({ outputUriPrefix, retryIfAlreadyExists: true })
   })
+
+async function exportDocuments(params: {
+  outputUriPrefix: string
+  retryIfAlreadyExists: boolean
+}) {
+  const { outputUriPrefix, retryIfAlreadyExists } = params
+  try {
+    await client.exportDocuments({
+      name: databaseName,
+      collectionIds: process.env.COLLECTION_IDS?.split(','),
+      outputUriPrefix: outputUriPrefix,
+    })
+    logger.info(`✅ Backup ${databaseName} to ${outputUriPrefix} successfully.`)
+  } catch (error) {
+    if (error instanceof HttpsError) {
+      logger.info(`${error.code}: ${error.message}: ${error.details}`)
+      if (retryIfAlreadyExists && error.code === 'invalid-argument') {
+        retryWithUniqueSuffix(outputUriPrefix)
+        return
+      }
+    }
+    logger.error(error, { structuredData: true })
+    throw new HttpsError('internal', '🚨 Backup operation failed.')
+  }
+}
+
+// Avoid object path name collisions
+async function retryWithUniqueSuffix(outputUriPrefix: string) {
+  const additionalSuffix = generateUniqueString()
+  await exportDocuments({
+    outputUriPrefix: `${outputUriPrefix}-${additionalSuffix}`,
+    retryIfAlreadyExists: false,
+  })
+}
+
+// Does not generate a completed unique string, but it is enough for this use case.
+function generateUniqueString() {
+  return Date.now().toString(36) + Math.random().toString(36).substring(2)
+}
