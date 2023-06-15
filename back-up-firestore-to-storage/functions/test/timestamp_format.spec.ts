4e62153c538e7cd50e9c825e857fa1b16c15db47
tsuruo <tsuru.dev@gmail.com>
2023-06-15 10:58:53 +0900

    Add mocha:watch test

---
+++ back-up-firestore-to-storage/functions/test/timestamp_format.spec.ts
@@ -0,0 +1,8 @@
+import { expect } from 'chai'
+import { describe, it } from 'mocha'
+
+describe('greet-the-world', () => {
+  it('1 + 1 should be 2', () => {
+    expect(1 + 1).to.equal(2)
+  })
+})
